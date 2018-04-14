d5cbe77fbbe9888b2621bbcac9c5f4f0ecb085c3
AlissonStopassole <alissonstopassole@hotmail.com>
2018-04-13 21:16:12 -0300

    retorna idade do usuario feito

--- br.com.oficinaSoftware.src/br/com/oficinaSoftware/controller/PerfilUsuarioController.java
+++ br.com.oficinaSoftware.src/br/com/oficinaSoftware/controller/PerfilUsuarioController.java
@@ -12,16 +12,24 @@ import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
 
 public class PerfilUsuarioController implements Initializable {
 
-	@FXML private TextField idNomeP;
-	@FXML private TextField idEnderecoP;
-	@FXML private TextField idCargoP;
-	@FXML private TextField idTelefoneP;
-	@FXML private TextField idEmailP;
-	@FXML private TextField idIdadeP;
+	@FXML
+	private TextField idNomeP;
+	@FXML
+	private TextField idEnderecoP;
+	@FXML
+	private TextField idCargoP;
+	@FXML
+	private TextField idTelefoneP;
+	@FXML
+	private TextField idEmailP;
+	@FXML
+	private TextField idIdadeP;
 
 	public static Usuario USUARIO_LOGADO;
 
@@ -32,10 +40,20 @@ public class PerfilUsuarioController implements Initializable {
 		idCargoP.setText(USUARIO_LOGADO.getCargo());
 		idTelefoneP.setText(USUARIO_LOGADO.getTelefone());
 		idEmailP.setText(USUARIO_LOGADO.getEmail());
-
+		Long idade = 0L;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			String hoje = sdf.format(new Date(System.currentTimeMillis()));
+			java.util.Date dataDe = sdf.parse(hoje);
+			java.util.Date dataAte = sdf.parse(USUARIO_LOGADO.getDataNasc());
+			idade = (((dataAte.getTime() - dataDe.getTime()) / (1000 * 60 * 60 * 24) / 30) / 12) * -1;
+		} catch (Exception e) {
+		}
+		idIdadeP.setText(String.valueOf(idade));
 	}
 
-	@FXML private void botaoVoltar (ActionEvent event) throws IOException {
+	@FXML
+	private void botaoVoltar(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("/br/com/oficinaSoftware/view/Menu.fxml"));
 		Scene scene = new Scene(root);
 		Main.myStage.setScene(scene);
